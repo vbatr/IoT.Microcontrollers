99ff2d04154ab86314c6b81ac6eedeee5e97d796
vbatr <volodymyr.batrukh@gmail.com>
2026-07-11 17:42:42 +0300

    Added simple example of blink app

---
+++ examples/blink.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // change state of the LED by setting the pin to the HIGH voltage level
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // change state of the LED by setting the pin to the LOW voltage level
+  delay(1000);                      // wait for a second
+}
